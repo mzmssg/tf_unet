a9d03818e6b2f1f4b90b522a1c0325cc2257208b
mzmssg <mzmssg@gmail.com>
2018-09-12 21:22:48 +0800

    seperate docker run

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
-docker run -it \
+docker run -d \
     --name tf_unet \
     -v $PWD:/root/tf_unet \
     -w /root/tf_unet \
     --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia2 --device=/dev/fuse \
     --volume /var/drivers/nvidia/current:/usr/local/nvidia:ro
-    mzmssg/eye /bin/bash 
+    mzmssg/eye sleep infinity
+
+docker exec -it tf_unet /bin/bash 
+
